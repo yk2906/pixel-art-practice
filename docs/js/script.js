18d32a149e34a3c2884b8117d9f2aca84eff36b7
yuto_kohama <yk050696@gmail.com>
2026-02-14 20:50:41 +0900

    test

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,16 +1,18 @@
-const gallery = document.getElementById('gallery');
-const totalImages = 2; // ここに今の合計枚数を書くだけ！
-
-for (let i = 1; i <= totalImages; i++) {
-  const card = document.createElement('div');
-  card.className = 'art-card';
-  card.innerHTML = `
-    <figure>
-      <img src="docs/images/practice_${i}.png" alt="作品${i}">
-    </figure>
-    <div class="info">
-      <h3>Practice #${i.toString().padStart(2, '0')}</h3>
-    </div>
-  `;
-  gallery.appendChild(card);
-}
+window.addEventListener('DOMContentLoaded', () => {
+    const gallery = document.getElementById('gallery');
+    const totalImages = 2; 
+  
+    for (let i = 1; i <= totalImages; i++) {
+      const card = document.createElement('div');
+      card.className = 'art-card';
+      card.innerHTML = `
+        <figure>
+          <img src="images/practice_${i}.png" alt="作品${i}">
+        </figure>
+        <div class="info">
+          <h3>Practice #${i.toString().padStart(2, '0')}</h3>
+        </div>
+      `;
+      gallery.appendChild(card);
+    }
+  });
